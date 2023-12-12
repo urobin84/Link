e69ca5f53e2c3af93e324a86e996a9be3d6456af
musholladarussalamid <musholladarussalamid@gmail.com>
2023-12-13 01:10:52 +0700

    implement base path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+  basePath: isProd ? '/Link' : '',
   output: 'export',
   distDir: 'dist',
   images:{
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 
 
 const Hero = () => {
+  const env = process.env.NODE_ENV === 'production';
+  const link = env ? '/Link' : '';
+
   return (
     <div className="max-container">
       <div className="flex p-2 justify-center ">
         <div className="box-content">
-          <Image className=" h-[270px]" src="/logo_musholladarussalam_vertical_white.png" alt="logo" />
+          <Image className=" h-[270px]" src={link +'/logo_musholladarussalam_vertical_white.png'} alt="logo" />
         </div>
       </div>
     </div>
